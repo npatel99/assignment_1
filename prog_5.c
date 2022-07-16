bb3e5df89d7f17f9cf932914d0e2af9fbf20e6f7
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-07-16 22:09:15 +0530

    return type of print()

---
+++ prog_5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*WAP to calculate the length of String using printf function*/
+int main() 
+{
+    int length;
+    /*printf() function return the count of char*/
+    length = printf("Narendra Patel");
+    printf("\nlength of string is: %d", length);
+    
+    return 0;
+}
