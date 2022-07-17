01415934cfd3467f3bc291ae1585abce097d4dab
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-07-17 08:43:11 +0530

    Update prog_10.c

--- prog_10.c
+++ prog_10.c
@@ -8,7 +8,7 @@ int main()
 {
     int DD, MM, YYYY;
     printf("Enter DD/MM/YYYY: ");
-    scanf("%d%d%d",&DD,&MM,&YYYY);
+    scanf("%d/%d/%d",&DD,&MM,&YYYY);
     printf("Day-%d, Month-%d, Year-%d", DD, MM, YYYY);
    
     return 0;
